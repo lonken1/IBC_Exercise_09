fd51741a806bc1ec0cfabb5228ebc75eca9e5ec4
lonken <lonken@nd.edu>
2019-11-26 16:25:15 -0500

    Finished Problem 1

--- ANSWERS.R
+++ ANSWERS.R
@@ -1,4 +1,17 @@
 # Clear Working Directory
 rm(list=ls())
 
+# Load in libraries
+library(ggplot2)
+library(cowplot)
+library(reshape2)
+
 # PROBLEM 1
+# We found a dataset in a .csv file online that had the height and weight for 5000 males
+
+# Load in file
+weightheight <- read.csv("weight-height.csv", header = T)
+
+graph <- ggplot(data=weightheight, aes(x=Height,y=Weight))
+graph+geom_point()+theme_classic()+stat_smooth(method = 'lm')
+
